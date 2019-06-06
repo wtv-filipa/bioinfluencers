9009e705f12bea6907284063304eaa313ac74571
Filipa <anafilipa.ferreira99@ua.pt>
2019-06-06 17:36:51 +0100

    menu de lado atualizado e a cena das notícias versão inicial

--- bioinfluencers/admin/componentes/noticias.php
+++ bioinfluencers/admin/componentes/noticias.php
@@ -3,36 +3,40 @@
     <p class="mb-4">Aqui é possível fazer update das notícias e verificar quais já se encontram publicadas, podendo ser atualizadas se necessário.</p>
     <div class="row">
         <div class="col-xl-12">
-            <form>
+
+
+            <!--FORM-->
+
+            <form method="post" action="scripts/inserir_noticias.php">
                 <div class="row">
-                <!--upload de imagem de capa da notícia-->
+                <!--upload de imagem de capa da notícia
                 <div class="form-group col-12">
                     <label class="text-gray-800" for="img1">Imagem principal</label>
                     <div class="file-upload-wrapper">
                         <input type="file" id="img1" class="file-upload" />
                     </div>
                 </div>
-
+-->
 
                 <!--colocar título da notícia-->
                 <div class="form-group col-xl-6 col-lg-6 col-sm-6">
                     <label class="text-gray-800" for="titulo">Título</label>
-                    <input type="text" class="form-control" id="titulo" placeholder="Indique o título">
+                    <input type="text" class="form-control" id="titulo" placeholder="Indique o título" name="titulo">
                 </div>
 
                 <!--colocar subtítulo da notícia-->
                 <div class="form-group col-xl-6 col-lg-6 col-sm-6">
                     <label class="text-gray-800" for="subtitulo">Subtítulo</label>
-                    <input type="text" class="form-control" id="subtitulo" placeholder="Indique o subtítulo">
+                    <input type="text" class="form-control" id="subtitulo" placeholder="Indique o subtítulo" name="subtitulo">
                 </div>
 
                 <!--colocar corpo da notícia-->
                 <div class="form-group col-12">
                     <label class="text-gray-800" for="corpo">Corpo da notícia</label>
-                    <textarea type="text" class="form-control" id="corpo" placeholder="Insira o texto relativo à notícia"></textarea>
+                    <textarea type="text" class="form-control" id="corpo" placeholder="Insira o texto relativo à notícia" name="texto"></textarea>
                 </div>
 
-                <!--upload de imagem adicional-->
+                <!--upload de imagem adicional
                 <div class="form-group col-12">
                     <label class="text-gray-800" for="img2">Imagem adicional</label>
                     <div class="file-upload-wrapper">
@@ -40,13 +44,15 @@
                     </div>
                 </div>
 
-                <!--upload de imagem adicional-->
+                <!--upload de imagem adicional
                 <div class="form-group col-12">
                     <label class="text-gray-800" for="img2">Imagem adicional</label>
                     <div class="file-upload-wrapper">
                         <input type="file" id="img2" class="file-upload" />
                     </div>
                 </div>
+-->
+
 
                 <div class="form-group col-3">
                     <button class="buttonCustomise"> Criar </button>
@@ -70,8 +76,7 @@
                         <th>Título</th>
                         <th>Subtitulo</th>
                         <th>Corpo da notícia</th>
-                        <th>Imagem princial</th>
-                        <th>Imagens secundárias</th>
+                        <th>Imagem</th>
                         <th>Ações</th>
                     </tr>
                     </thead>
@@ -80,8 +85,7 @@
                         <th>Título</th>
                         <th>Subtitulo</th>
                         <th>Corpo da notícia</th>
-                        <th>Imagem princial</th>
-                        <th>Imagens secundárias</th>
+                        <th>Imagem</th>
                         <th>Ações</th>
                     </tr>
                     </tfoot>
@@ -93,63 +97,35 @@
                     $link= new_db_connection(); //Create a new DB connection
                     $stmt = mysqli_stmt_init($link); //create a prepared statement
 
-                    $query = "SELECT id_users, email, username, date_creation, active, roles_descricao FROM users INNER JOIN roles ON users.ref_id_roles= roles.id_roles WHERE username LIKE ? OR email LIKE ? "; // Define the query
+                    $query = "SELECT id_noticias, titulo, subtitulo, texto, data_hora FROM noticias"; // Define the query
+                    if (mysqli_stmt_prepare($stmt, $query)) { // Prepare the statement
+                        mysqli_stmt_execute($stmt); // Execute the prepared statement
 
+                        mysqli_stmt_bind_result($stmt, $id, $title, $subtitle, $text, $date); // Bind results
+
+
+                        while (mysqli_stmt_fetch($stmt)) {
+                            echo "<tbody>
+                    <tr>
+                        <td>$title</td>
+                        <td>$subtitle</td>
+                        <td>$text</td>
+                        <td></td>
+                        <td>
+                            <i class=\"fas fa-trash\"></i>
+                            <i class=\"fas fa-ban\"></i>
+                            <i class=\"fas fa-edit\"></i></td>
+                    </tr>
+
+                    </tbody>";
+
+                        }
+                    }
 
                     ?>
 
 
-                    <tbody>
-                    <tr>
-                        <td>100 pessoas ajudaram na recolha de beatas na UA</td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. </td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris</td>
-                        <td>IMG_03.jpg</td>
-                        <td>IMG_03.jpg</td>
-                        <td>
-                            <i class="fas fa-trash"></i>
-                            <i class="fas fa-ban"></i>
-                            <i class="fas fa-edit"></i></td>
-                    </tr>
 
-                    <tr>
-                        <td>Plantar àrvores é a nova atividade do jovens</td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. </td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris</td>
-                        <td>IMG_03.jpg</td>
-                        <td>IMG_03.jpg</td>
-                        <td>
-                            <i class="fas fa-trash"></i>
-                            <i class="fas fa-ban"></i>
-                            <i class="fas fa-edit"></i></td>
-                    </tr>
-
-                    <tr>
-                        <td>UA dinamiza a adoção de uma àrvore</td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. </td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris</td>
-                        <td>IMG_03.jpg</td>
-                        <td>IMG_03.jpg</td>
-                        <td>
-                            <i class="fas fa-trash"></i>
-                            <i class="fas fa-ban"></i>
-                            <i class="fas fa-edit"></i></td>
-                    </tr>
-
-                    <tr>
-                        <td>Mais de 5000 beatas foram apanhadas em 30 minutos</td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. </td>
-                        <td>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris</td>
-                        <td>IMG_03.jpg</td>
-                        <td>IMG_03.jpg</td>
-                        <td>
-                            <i class="fas fa-trash"></i>
-                            <i class="fas fa-ban"></i>
-                            <i class="fas fa-edit"></i></td>
-                    </tr>
-
-
-                    </tbody>
                 </table>
             </div>
         </div>
