21bd7fd91d23fc12b8ca5c497c807b7fce065777
Filipa <anafilipa.ferreira99@ua.pt>
2019-06-12 17:11:00 +0100

    notícias by ana

---
+++ bioinfluencers/public/componentes/noticias.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="fundo_noticias text-center">
+            <div>
+                <i class="fa fa-newspaper-o" style="font-size: 2rem; color: white"></i>
+            </div>
+            <p>Notícias</p>
+        </div>
+
+    </div>
+
+
+
+
+
+</div>
---
+++ bioinfluencers/public/noticias.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- metadados -->
+    <?php include "helpers/meta.php"; ?>
+
+    <title>Notícias</title>
+
+    <!-- Custom fonts for this template-->
+    <?php include "helpers/fonts.php"; ?>
+
+    <!-- Custom styles for this template-->
+
+    <?php include "helpers/css.php"; ?>
+
+</head>
+
+
+<body>
+<header class="sticky-top ">
+
+    <?php include "componentes/navbar.php"; ?>
+
+</header>
+
+<main class="p-0 mb-5">
+
+    <?php include "componentes/noticias.php"; ?>
+
+</main>
+
+<!-- JavaScript-->
+
+<?php include "helpers/js.php"; ?>
+
+</body>
+
+</html>
