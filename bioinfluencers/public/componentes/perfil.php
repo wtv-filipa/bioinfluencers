3cba899590d4695c7f7c12b09f9bf23e3fbb7ad3
Ana <anajvaz@ua.pt>
2019-06-12 17:03:15 +0100

    perfil começado, alteração de css também

--- bioinfluencers/public/componentes/perfil.php
+++ bioinfluencers/public/componentes/perfil.php
@@ -1,20 +1,31 @@
 
 <div class="container">
 
-    <div class="row text-center">
+    <div class="row text-center topo">
 
-        <div class="col-4">
+        <div class="col-4 text-right centro">
             <h5>2345 <br> Seguidores</h5>
         </div>
 
         <div class="col-4">
-            <img class="img-fluid img_redonda" src="img/pessoa2.jpg">
+            <img width="190px" height="170px" class=" img_redonda" src="img/pessoa2.jpg">
         </div>
 
-        <div class="col-4">
+        <div class="col-4 text-left centro">
             <h5>2345 <br> A seguir</h5>
         </div>
 
+    </div>
+
+    <div class="text-center utilizador topo1">
+
+        <h4>Cristina Costa</h4>
+        <p>30775 pontos</p>
+
+    </div>
+
+    <div>
+        <p>Lorem ipsum dolor sit amet, ea constituam voluptatibus est, vitae vidisse recteque ea eam. Iudico oblique fabellas an vis, eos eirmod constituam ad, ex delicata oportere signiferumque eum. </p>
     </div>
 
 
