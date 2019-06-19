5642f7eddd052f9380c19c47c60475352cdefc53
Fred <fredericoproenca@ua.pt>
2019-06-19 15:21:09 +0100

    A FDP DA NAV

--- bioinfluencers/public/componentes/navbar.php
+++ bioinfluencers/public/componentes/navbar.php
@@ -4,58 +4,57 @@ if (isset($_SESSION["nickname"]) && isset($_SESSION["tipo"])) {
     $tipo = $_SESSION["tipo"];
 }
 ?>
-<nav class="navbar navbar-default navbar-expand-xl navbar-light sticky-top">
-    <div class="container">
-    <div class="navbar-header d-flex col">
-        <a class="navbar-brand" href="#"><img src="img/logo_n.png" class=""></a>
 
-        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-toggler ml-auto">
-            <span class="navbar-toggler-icon"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-    </div>
+<header class="top ">
+    <nav class="nav__ navfont">
 
-    <!-- Collection of nav links, forms, and other content for toggling -->
-    <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
-        <ul class="nav navbar-nav">
-            <li class="nav-item active"><a href="index.php" class="nav-link"><i class="fa fa-picture-o" style="font-size: 1.3rem"></i></a></li>
-            <li class="nav-item"><a href="eventos.php" class="nav-link"><i class="fa fa-calendar-o" style="font-size: 1.3rem"></i></a></li>
-            <li class="nav-item"><a href="top.php" class="nav-link"><i class="fa fa-sitemap" style="font-size: 1.3rem"></i></a></li>
-            <li class="nav-item"><a href="noticias.php" class="nav-link"><i class="fa fa-newspaper-o" style="font-size: 1.3rem"></i></a></li>
-            <li class="nav-item"><a href="grupos.php" class="nav-link"><i class="fa fa-star-o" style="font-size: 1.3rem"></i></a></li>
-            <li class="nav-item"><a href="codigo_evento.php" class="nav-link"><i class="fa fa-star-o" style="font-size: 1.3rem"></i></a></li>
 
-        </ul>
-        <form class="navbar-form form-inline">
-            <div class="input-group search-box w-50">
-                <input type="text" id="search" class="form-control text-left" placeholder="Pesquisar...">
-                <span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+        <div class="nav__controls--left">
+
+            <div class="nav__avatar"><img src="img/menu.png" alt="" class="icones nav__link--icon"/>
+                    <div class="nav__avatar--dropdown"><a href=""></a>
+                        <li class="nav__item nav__item--active"><a class="nav__link mr-2" href="index.php"><img src="img/feed.png" alt="" class="icones nav__link--icon"/><span class="nav__link--text">Feed</span></a>
+                        <li class="nav__item"><a class="nav__link mr-2" href="eventos.php"><img src="img/eventos_icon.png" alt="" class="icones nav__link--icon"/></span><span class="nav__link--text">Eventos</span></a></li>
+                        <li class="nav__item"><a class="nav__link mr-2" href="top.php"><img src="img/top.png" alt="" class="icones nav__link--icon"/><span class="nav__link--text">Top</span></a></li>
+                        <li class="nav__item"><a class="nav__link mr-2" href="noticias.php"><img src="img/noticias.png" alt="" class="icones nav__link--icon"/><span class="nav__link--text">Notícias</span></a></li>
+                        <li class="nav__item"><a class="nav__link mr-2" href="grupos.php"><img src="img/grupos.png" alt="" class="icones nav__link--icon"/><span class="nav__link--text">Grupos</span></a></li>
+                        <li class="nav__item"><a class="nav__link mr-2" href="codigo_evento.php"><img src="img/med_trof.png" alt="" class="icones nav__link--icon"/><span class="nav__link--text">Inserir código</span></a></li>
+                        </li>
+                    </div></i>
             </div>
-        </form>
-        <ul class="nav navbar-nav navbar-right ml-auto">
-            <li class="nav-item"><a href="#" class="nav-link notifications"><i class="fa fa-bell-o"></i><span class="badge">1</span></a></li>
-            <li class="nav-item dropdown">
-                <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle user-action"><img src="img/pessoa2.jpg" class="avatar" alt="Avatar"><?= $nickname ?><b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <?php
-                    if(isset($tipo) && $tipo == 1) {
-                        ?>
-                        <li><a href="../admin/index.php" class="dropdown-item" style="font-weight: bold"><i class="fa fa-shield"></i>ADMIN</a></li>
-                        <?php
-                    }
-                    ?>
+        </div>
 
-                    <li class="divider dropdown-divider"></li>
-                    <li><a href="perfil.php" class="dropdown-item"><i class="fa fa-user-o"></i>Ver perfil</a></li>
-                    <li><a href="codigo_utilizador.php" class="dropdown-item"><i class="fa fa-star-o"></i> Código</a></li>
-                    <li><a href="definicoes.php" class="dropdown-item"><i class="fa fa-sliders"></i>Definições</a></li>
-                    <li class="divider dropdown-divider"></li>
-                    <li><a href="scripts/logout.php" class="dropdown-item"><i class="material-icons">&#xE8AC;</i> Logout</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    </div>
-</nav>
+
+        <div class="nav__brand"><a class="nav__link" href="/"><img src="img/logo_n.png" class="" style="width:150px"></a></div>
+        <div class="nav__controls nav__controls--right">
+            <div class="nav__search">
+                <input class="nav__search--input text-center" placeholder="" type="text"/><a class="nav__search--icon " href="#"><i class="material-icons">search</i></a>
+            </div>
+
+            <div class="nav__avatar"><img src="img/pessoa2.jpg" class="nav__avatar--image" style="width:35px">
+
+
+
+                    <div class="nav__avatar--dropdown"><a href="/login"></a>
+                        <?php
+                        if(isset($tipo) && $tipo == 1) {
+                            ?>
+                            <button class="nav__btn2" style="background: #7FC53C;"><i class="fa fa-shield"></i> ADMIN</button><a href="../admin/index.php"></a>
+                            <?php
+                        }
+                        ?>
+                        <a href="perfil.php"><button class="nav__btn2"> <!--<i class="fa fa-user mr-2">--> Ver perfil</button></a>
+                        <a href="codigo_utilizador.php"><button class="nav__btn2"> <!--<i class="fa fa-star-o mr-2"></i>-->Código</button></a>
+                        <a href="definicoes.php"><button class="nav__btn2"> <!--<i class="fa fa-sliders mr-2"></i>-->Definições</button></a>
+                        <a href="scripts/logout.php"><button class="nav__btn2"><i class="fa fa-sign-out mr-2"></i>Logout</button></a>
+
+                    </div>
+            </div>
+            <span class="" style="font-size: 13px; color: black"><?=$nickname?></span>
+        </div>
+    </nav>
+</header>
+
+
+
+
