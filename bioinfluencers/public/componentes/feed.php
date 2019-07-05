3c00efca913721320db19c4dc2d26b2e756b1bb2
Ana <anajvaz@ua.pt>
2019-07-05 14:21:03 +0100

    coisas Ana dia 5

--- bioinfluencers/public/componentes/feed.php
+++ bioinfluencers/public/componentes/feed.php
@@ -8,10 +8,10 @@
 
     $query = "SELECT id_partilhas, data_hora, descricao, utilizadores_id_utilizadores, id_utilizadores, nickname, img_perfil, filename, partilhas_id_partilhas
               FROM partilhas
+              LEFT JOIN conteudos
+              ON partilhas.id_partilhas = conteudos.partilhas_id_partilhas
               INNER JOIN utilizadores
               ON partilhas.utilizadores_id_utilizadores = utilizadores.id_utilizadores
-              INNER JOIN conteudos
-              ON partilhas.id_partilhas = conteudos.partilhas_id_partilhas
               ORDER BY data_hora DESC";
 
     if (mysqli_stmt_prepare($stmt, $query)) {
@@ -21,7 +21,6 @@
 
         while (mysqli_stmt_fetch($stmt)) {
             ?>
-
             <div class="row no-gutters">
                 <div class="card test">
                     <div class="header">
@@ -39,7 +38,7 @@
                             ?>
                             <div class="headers-text ml-2">
                                 <span class="author-name"><?= $nome_u ?></span>
-                                <span class="header-secondary-text"><?= substr($data_hora, 11) ?></span>
+                                <span class="header-secondary-text"><?= substr($data_hora, 10, 6) ?></span>
                             </div>
                         </div>
                         <i class="material-icons dropdown-icon">expand_more</i>
@@ -48,13 +47,23 @@
                     <div class="text-content">
                         <p><?= $descricao ?></p>
                     </div>
-                    <div class="photo-content">
-                        <div class="photo two">
-                            <img class="card-photo" src="../admin/uploads/publicacao/<?= $filename ?>"/>
+                    <?php
+                    if (isset($partilhas_id_partilhas)) {
+                        echo "ESTA A DAR!"
+                        ?>
+
+                        <div class="photo-content">
+                            <div class="photo two">
+                                <img class="card-photo" src="../admin/uploads/publicacao/<?= $filename ?>"/>
+                            </div>
                         </div>
-                    </div>
-
-
+                        <?php
+                    } else {
+                        ?>
+                        <div></div>
+                        <?php
+                    }
+                    ?>
                     <div class="actions">
                         <div class="like-wrapper">
                             <i class="fa fa-leaf" aria-hidden="true" style="font-size: 1.5rem"></i>
@@ -74,112 +83,8 @@
             </div>
             <?php
         }
-
-    }
-
-    $query = "SELECT id_partilhas, data_hora, descricao, utilizadores_id_utilizadores, id_utilizadores, nickname, img_perfil
-              FROM partilhas
-              INNER JOIN utilizadores
-              ON partilhas.utilizadores_id_utilizadores = utilizadores.id_utilizadores
-              ORDER BY data_hora DESC";
-
-    if (mysqli_stmt_prepare($stmt, $query)) {
-
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $id_partilhas, $data_hora, $descricao, $uti_id_uti, $id_utilizadores, $nome_u, $img_perfil);
-
-        while (mysqli_stmt_fetch($stmt)) {
-            ?>
-
-            <div class="row no-gutters">
-                <div class="card test">
-                    <div class="header">
-                        <div class="right-side">
-                            <?php
-                            if (isset($img_perfil)) {
-                                ?>
-                                <img src="../admin/uploads/img_perfil/<?= $img_perfil ?>" class="avatar"/>
-                                <?php
-                            } else {
-                                ?>
-                                <img src="img/default.gif" class="avatar"/>
-                                <?php
-                            }
-                            ?>
-                            <div class="headers-text ml-2">
-                                <span class="author-name"><?= $nome_u ?></span>
-                                <span class="header-secondary-text"><?= substr($data_hora, 11) ?></span>
-                            </div>
-                        </div>
-                        <i class="material-icons dropdown-icon">expand_more</i>
-                    </div>
-
-                    <div class="text-content">
-                        <p><?= $descricao ?></p>
-                    </div>
-
-                    <div class="actions">
-                        <div class="like-wrapper">
-                            <i class="fa fa-leaf" aria-hidden="true" style="font-size: 1.5rem"></i>
-                            <span class="like-napis-dla-rafala">
-          Gosto
-        </span>
-                        </div>
-                        <div class="social-wrapper">
-                            <!--<a href="#" class="comment-count">16 comments</a>-->
-                            <i class="fa fa-share" aria-hidden="true" style="font-size: 1.2rem"></i>
-                            <span class="like-napis-dla-rafala pl-1">
-         Partilhar
-        </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php
-        }
-
     }
     ?>
-
-
-    <div class="row no-gutters">
-        <div class="card test">
-            <div class="header">
-                <div class="right-side">
-                    <img src="img/pessoa4.jpg" class="avatar"/>
-                    <div class="headers-text">
-                        <span class="author-name">Rui Ferreira</span>
-                        <span class="header-secondary-text">Há 22 min.</span>
-                    </div>
-                </div>
-                <i class="material-icons dropdown-icon">expand_more</i>
-            </div>
-            <div class="text-content">
-                <p>
-                    <span style="font-weight: bold;">Comentou</span> "Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit.
-                    Sed fringilla et turpis in condimentum. Sed ac turpis massa." <span style="font-weight: bold;">em #desflorestação.</span>
-
-                </p>
-            </div>
-            <div class="actions">
-                <div class="like-wrapper">
-                    <i class="fa fa-leaf" aria-hidden="true" style="font-size: 1.5rem"></i>
-                    <span class="like-napis-dla-rafala">
-          Gosto
-        </span>
-                </div>
-                <div class="social-wrapper">
-                    <a href="#" class="comment-count">16 comments</a>
-                    <i class="fa fa-share" aria-hidden="true" style="font-size: 1.2rem"></i>
-                    <span class="like-napis-dla-rafala pl-1">
-         Partilhar
-        </span>
-                </div>
-            </div>
-        </div>
-    </div>
-
 
     <a href="criar_publicacao.php" class="float">
         <i class="fa  fa-pencil-square-o my-float"></i>
@@ -236,4 +141,45 @@
             </div>
         </div>
     </div>
+
+    <div class="row no-gutters">
+        <div class="card test">
+            <div class="header">
+                <div class="right-side">
+                    <img src="img/pessoa4.jpg" class="avatar"/>
+                    <div class="headers-text">
+                        <span class="author-name">Rui Ferreira</span>
+                        <span class="header-secondary-text">Há 22 min.</span>
+                    </div>
+                </div>
+                <i class="material-icons dropdown-icon">expand_more</i>
+            </div>
+            <div class="text-content">
+                <p>
+                    <span style="font-weight: bold;">Comentou</span> "Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit.
+                    Sed fringilla et turpis in condimentum. Sed ac turpis massa." <span style="font-weight: bold;">em #desflorestação.</span>
+
+                </p>
+            </div>
+            <div class="actions">
+                <div class="like-wrapper">
+                    <i class="fa fa-leaf" aria-hidden="true" style="font-size: 1.5rem"></i>
+                    <span class="like-napis-dla-rafala">
+          Gosto
+        </span>
+                </div>
+                <div class="social-wrapper">
+                    <a href="#" class="comment-count">16 comments</a>
+                    <i class="fa fa-share" aria-hidden="true" style="font-size: 1.2rem"></i>
+                    <span class="like-napis-dla-rafala pl-1">
+         Partilhar
+        </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 -->
